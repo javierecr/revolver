2395bc4db41f6b9c3c05c59bb493dae4ef79a5d6
Javier Eduardo <javierecr@gmail.com>
2022-05-07 16:36:49 -0500

    Add test pop up modal

--- assets/index.js
+++ assets/index.js
@@ -34498,3 +34498,31 @@
       });
   },
 ]);
+
+
+// VIDEO POP UP
+
+$(document).ready(function () {
+  $(window).load(function () {
+    $duration = 15;
+    $('.seconds').text($duration);
+    $('.popup-wrap').fadeIn(1500);
+
+    $myTimer = setInterval(function () {
+      startTimer();
+    }, 1000);
+    $('.popup .btn-close').on('click', function () {
+      clearInterval($myTimer);
+      $('.popup-wrap').fadeOut(500);
+    });
+
+    function startTimer() {
+      $duration--;
+      $('.seconds').text($duration);
+      if ($duration == 0) {
+        clearInterval($myTimer);
+        $('.popup-wrap').fadeOut(500);
+      }
+    }
+  });
+});
