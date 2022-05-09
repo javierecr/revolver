1ca19a8fbcfcc0e927014fd02d425bf964aa5116
Javier Eduardo <javierecr@gmail.com>
2022-05-09 16:40:35 -0500

    Fix js for video modal triggers

--- assets/video-popup.js
+++ assets/video-popup.js
@@ -1,10 +1,6 @@
-const modalTriggers = document.querySelectorAll('.popup-trigger');
-const modalCloseTrigger = document.querySelector('.popup-modal__close');
 const bodyBlackout = document.querySelector('.body-blackout');
 
-function openVideoModal(videoUrl, modalId) {
-  const videoPlayer = document.getElementById(`modal-video-player-${modalId}`);
-  const videoSource = document.getElementById(`modal-video-source-${modalId}`);
+function openVideoModal(modalId) {
   const popupModal = document.getElementById(modalId);
 
   popupModal.classList.add('is--visible');
@@ -13,10 +9,7 @@ function openVideoModal(videoUrl, modalId) {
 
 function closeVideoModal(modalId) {
   const videoPlayer = document.getElementById(`modal-video-player-${modalId}`);
-  const videoSource = document.getElementById(`modal-video-source-${modalId}`);
-  console.log('modalId', modalId);
   const popupModal = document.getElementById(modalId);
-  console.log('popupModal', popupModal);
 
   popupModal.classList.remove('is--visible');
   bodyBlackout.classList.remove('is-blacked-out');
