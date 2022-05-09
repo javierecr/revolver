8ceef7856d120c2359af509b76dd08b8ba261fb5
Javier Eduardo <javierecr@gmail.com>
2022-05-09 13:58:09 -0500

    Fix js for video modal

--- assets/video-popup.js
+++ assets/video-popup.js
@@ -22,7 +22,7 @@ modalTriggers.forEach((trigger) => {
         console.log('close modal');
         videoPlayer.pause();
         videoSource.removeAttribute('src'); // empty source
-        videoSource.load();
+        videoPlayer.load();
         popupModal.classList.remove('is--visible');
         bodyBlackout.classList.remove('is-blacked-out');
       });
