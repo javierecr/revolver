299902fdf02054206061d63b9c3561a313507485
Javier Eduardo <javierecr@gmail.com>
2022-05-09 15:59:09 -0500

    Test JS in video modal

--- assets/video-popup.js
+++ assets/video-popup.js
@@ -19,13 +19,10 @@ modalTriggers.forEach((trigger) => {
     popupModal
       .querySelector('.popup-modal__close')
       .addEventListener('click', () => {
-        console.log('close modal');
-        console.log('videoplayer', videoPlayer);
-        console.log('videosource', videoSource);
         videoPlayer.pause();
         videoSource.removeAttribute('src'); // empty source
-        popupModal.classList.remove('is--visible');
-        bodyBlackout.classList.remove('is-blacked-out');
+        // popupModal.classList.remove('is--visible');
+        // bodyBlackout.classList.remove('is-blacked-out');
       });
 
     bodyBlackout.addEventListener('click', () => {
