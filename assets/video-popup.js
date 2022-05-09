b4cf0d015f3d624001f926af4e12d128af2345d3
Javier Eduardo <javierecr@gmail.com>
2022-05-09 16:35:31 -0500

    Fix interaction with video modal

--- assets/video-popup.js
+++ assets/video-popup.js
@@ -2,47 +2,13 @@ const modalTriggers = document.querySelectorAll('.popup-trigger');
 const modalCloseTrigger = document.querySelector('.popup-modal__close');
 const bodyBlackout = document.querySelector('.body-blackout');
 
-modalTriggers.forEach((trigger) => {
-  trigger.addEventListener('click', () => {
-    const { popupTrigger } = trigger.dataset;
-    console.log('popupTrigger', popupTrigger);
-    const popupModal = document.querySelector(
-      `[data-popup-modal="${popupTrigger}"]`
-    );
-
-    popupModal.classList.add('is--visible');
-    bodyBlackout.classList.add('is-blacked-out');
-
-    var videoPlayer = document.getElementById(
-      `modal-video-player-${popupTrigger}`
-    );
-    var videoSource = document.getElementById(
-      `modal-video-source-${popupTrigger}`
-    );
-
-    popupModal
-      .querySelector('.popup-modal__close')
-      .addEventListener('click', () => {
-        videoPlayer.pause();
-        videoSource.removeAttribute('src'); // empty source
-        videoPlayer.load();
-        popupModal.classList.remove('is--visible');
-        bodyBlackout.classList.remove('is-blacked-out');
-      });
-
-    bodyBlackout.addEventListener('click', () => {
-      popupModal.classList.remove('is--visible');
-      bodyBlackout.classList.remove('is-blacked-out');
-    });
-  });
-});
-
 function openVideoModal(videoUrl, modalId) {
   const videoPlayer = document.getElementById(`modal-video-player-${modalId}`);
   const videoSource = document.getElementById(`modal-video-source-${modalId}`);
   const popupModal = document.getElementById(modalId);
 
   popupModal.classList.add('is--visible');
+  bodyBlackout.classList.add('is-blacked-out');
 }
 
 function closeVideoModal(modalId) {
@@ -53,5 +19,6 @@ function closeVideoModal(modalId) {
   console.log('popupModal', popupModal);
 
   popupModal.classList.remove('is--visible');
+  bodyBlackout.classList.remove('is-blacked-out');
   videoPlayer.pause();
 }
