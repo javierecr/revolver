468309c36a7e86c91a9625409ca9772b3d682189
Javier Eduardo <javierecr@gmail.com>
2022-05-09 11:04:50 -0500

    Fix liquid error

--- assets/video-popup.js
+++ assets/video-popup.js
@@ -16,6 +16,7 @@ modalTriggers.forEach((trigger) => {
     popupModal
       .querySelector('.popup-modal__close')
       .addEventListener('click', () => {
+        console.log('close modal');
         popupModal.classList.remove('is--visible');
         bodyBlackout.classList.remove('is-blacked-out');
       });
